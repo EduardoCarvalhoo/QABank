00ba4f88bef4e8d832a7a8a4d5b01d8dea49d6ad
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2025-08-23 13:52:26 -0300

    feat: password registration step screen

--- app/src/main/java/com/example/qabank/fragment/SavingInformationFragment.kt
+++ app/src/main/java/com/example/qabank/fragment/SavingInformationFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import com.example.qabank.PasswordRegistrationStepFragment
+import com.example.qabank.activity.AccountOpeningActivity
 import com.example.qabank.databinding.FragmentSavingInformationBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SavingInformationFragment : Fragment() {
 
@@ -17,6 +22,16 @@ class SavingInformationFragment : Fragment() {
     ): View? {
         _binding = FragmentSavingInformationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(4000)
+            val activity = requireActivity() as AccountOpeningActivity
+            activity.navigateTo(PasswordRegistrationStepFragment())
+        }
     }
 
     override fun onDestroy() {
