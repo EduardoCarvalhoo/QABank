8da33a8e96e8aeb9561e956d0699706688992a8d
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2025-08-22 23:25:07 -0300

    feat: saving information screen

--- app/src/main/java/com/example/qabank/fragment/IncomeFragment.kt
+++ app/src/main/java/com/example/qabank/fragment/IncomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.qabank.activity.AccountOpeningActivity
 import com.example.qabank.databinding.FragmentIncomeBinding
 
 class IncomeFragment : Fragment() {
@@ -17,5 +18,18 @@ class IncomeFragment : Fragment() {
     ): View? {
         _binding = FragmentIncomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentIncomeAdvanceButton.setOnClickListener {
+            val activity = requireActivity() as AccountOpeningActivity
+            activity.navigateTo(SavingInformationFragment())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/qabank/fragment/SavingInformationFragment.kt
@@ -0,0 +1,26 @@
+package com.example.qabank.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.qabank.databinding.FragmentSavingInformationBinding
+
+class SavingInformationFragment : Fragment() {
+
+    private var _binding: FragmentSavingInformationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSavingInformationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
