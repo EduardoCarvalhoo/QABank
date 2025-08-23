e8bf920478027f459548981708668786c5eb7324
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2025-08-22 22:31:17 -0300

    feat: address registration screen

---
+++ app/src/main/java/com/example/qabank/fragment/AddressRegistrationFragment.kt
@@ -0,0 +1,21 @@
+package com.example.qabank.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.qabank.databinding.FragmentAddressRegistrationBinding
+
+class AddressRegistrationFragment : Fragment() {
+
+    private var _binding: FragmentAddressRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddressRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
