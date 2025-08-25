5296c48498e562d948757b56c3e2fb4edef7e99e
EduardoCarvalhoo <eduardo-carvalho11@hotmail.com>
2025-08-25 19:52:54 -0300

    feat: password confirmation screen

--- app/src/main/java/com/example/qabank/PasswordRegistrationFragment.kt
+++ app/src/main/java/com/example/qabank/PasswordRegistrationFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.qabank.activity.AccountOpeningActivity
+import com.example.qabank.databinding.FragmentPasswordRegistrationBinding
 
 class PasswordRegistrationFragment : Fragment() {
 
+    private var _binding: FragmentPasswordRegistrationBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_password_registration, container, false)
+        _binding = FragmentPasswordRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentPasswordRegistrationAdvanceButton.setOnClickListener {
+            val activity = requireActivity() as AccountOpeningActivity
+            activity.navigateTo(PasswordConfirmationFragment())
+        }
     }
 }
